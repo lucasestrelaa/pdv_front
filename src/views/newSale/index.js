7735f786ff11eac88ebfa31cd8eb90352b382be2
unknown <lucase.o.melo14@gmail.com>
2023-10-22 17:32:12 -0300

    carrinho de produtos

--- src/views/newSale/index.js
+++ src/views/newSale/index.js
@@ -1,9 +1,28 @@
 import { useState, useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+// import { useTheme } from '@mui/material/styles';
+
 
 // material-ui
-import { Typography, Grid, Button, Input } from '@mui/material';
+import {  styled } from '@mui/material/styles';
+
+import {
+  Divider,
+  Avatar,
+  Typography, 
+  Grid, 
+  Button, 
+  Input, 
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText
+} from '@mui/material';
+import { gridSpacing } from 'store/constant';
+import User1 from 'assets/images/users/user-round.svg';
+
+
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
@@ -11,9 +30,22 @@ import axios from 'axios';
 
 import { SET_PRODUCT } from 'store/actions';
 
+// styles
+const ListItemWrapper = styled('div')(({ theme }) => ({
+  cursor: 'pointer',
+  padding: 16,
+  '&:hover': {
+    background: theme.palette.primary.light
+  },
+  '& .MuiListItem-root': {
+    padding: 0
+  }
+}));
+
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const NewSale = () => {
+  // const theme = useTheme();
   const dispatch = useDispatch();
   const [data, setData] = useState([]);
   // const [typePayment, setTypePayment] = useState('');
@@ -23,7 +55,10 @@ const NewSale = () => {
   // const [price, setPrice] = useState('');
   const token = sessionStorage.getItem('authorization');
   const urlBase = sessionStorage.getItem('UrlBase');
+  const products = useSelector((state) => state.customization.product);
+
   /**
+   * 
    * Todas as vendas, puxar os dados dos produtos/vendas
    */
   useEffect(() => {
@@ -71,59 +106,111 @@ const NewSale = () => {
 
 
 
+
+
   return (
-    <MainCard title="Vendas">
-      Produtos da loja
-      {data.length > 0 && (
-        <Grid container direction="column" style={{ paddingTop: 10 }}>
-          {data.map((res, index) => {
-            return (
-              <Grid container key={`${index}`} direction="column">
-                <Grid item>
-                  <Grid container alignItems="center" justifyContent="space-between">
-                    <Grid item>
-                      <Typography variant="subtitle1" color="inherit">
-                        {res.name}
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Grid container alignItems="center" justifyContent="space-between">
-                        <Grid item style={{ marginRight: '10px' }}>
-                          <Typography variant="subtitle1" color="inherit">
-                            R$ {res.price}
-                          </Typography>
-                        </Grid>
-                        <Grid item>
-                          <Typography variant="subtitle1" color="inherit">
-                            <Input
-                              defaultValue={0}
-                              sx={{ width: 40 }}
-                              type="number"
-                              id={`qnt${index}`}
-                              aria-describedby="my-helper-text"
-                              onChange={(e) => handleQnt(index, e.target.value)}
-                            />
-                          </Typography>
-                        </Grid>
-                        <Grid item>
-                          <Typography variant="subtitle1" color="inherit">
-                            <Button onClick={() => Add(res)}>Adicionar produto</Button>
-                          </Typography>
+
+    <MainCard xs={8} title="Vendas">
+
+      <Grid container spacing={gridSpacing}>
+        {data.length > 0 && (
+          <Grid container xs={6} direction="column" style={{ padding: 10 }}>
+            Produtos da loja
+            {data.map((res, index) => {
+              return (
+                <Grid container key={`${index}`} direction="column">
+                  <Grid item>
+                    <Grid container alignItems="center" justifyContent="space-between">
+                      <Grid item>
+                        <Typography variant="subtitle1" color="inherit">
+                          {res.name}
+                        </Typography>
+                      </Grid>
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item style={{ marginRight: '10px' }}>
+                            <Typography variant="subtitle1" color="inherit">
+                              R$ {res.price}
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="subtitle1" color="inherit">
+                              <Input
+                                defaultValue={0}
+                                sx={{ width: 40 }}
+                                type="number"
+                                id={`qnt${index}`}
+                                aria-describedby="my-helper-text"
+                                onChange={(e) => handleQnt(index, e.target.value)}
+                              />
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="subtitle1" color="inherit">
+                              <Button onClick={() => Add(res)}>Adicionar produto</Button>
+                            </Typography>
+                          </Grid>
                         </Grid>
                       </Grid>
                     </Grid>
                   </Grid>
+                  <Grid item>
+                    <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                      {res.color}
+                    </Typography>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
-                    {res.color}
-                  </Typography>
+              );
+            })}
+          </Grid>
+        )}
+        <Grid container xs={6} style={{ padding: 10 }}>
+          Carrinho
+          {products.map((res, index) => (
+            <Grid key={`${index}`}>
+              <ListItemWrapper>
+                <ListItem alignItems="center">
+                  <ListItemAvatar>
+                    <Avatar alt="John Doe" src={User1} />
+                  </ListItemAvatar>
+                  <ListItemText primary={`${res.name}`} />
+                  <ListItemSecondaryAction>
+                    <Grid container justifyContent="flex-end">
+                      <Grid item xs={12}>
+                        <Typography variant="caption" display="block" gutterBottom>
+                          {res.quantidade}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </ListItemSecondaryAction>
+                </ListItem>
+                <Grid container direction="column" className="list-container">
+                  <Grid item xs={12} sx={{ pb: 2 }}>
+                    <Typography variant="subtitle2">Preço: R${(parseFloat(res.price) * parseInt(res.quantidade).toFixed(2))}</Typography>
+                  </Grid>
+
+                  <Grid item xs={12} sx={{ pb: 2 }}>
+                    <Typography variant="subtitle2">Cor: {`${res.color}`}</Typography>
+                  </Grid>
+                  {/* <Grid item xs={12}>
+                <Grid container>
+                  <Grid item>
+                    <Chip label="Unread" sx={chipErrorSX} />
+                  </Grid>
+                  <Grid item>
+                    <Chip label="New" sx={chipWarningSX} />
+                  </Grid>
                 </Grid>
-              </Grid>
-            );
-          })}
+              </Grid> */}
+                </Grid>
+              </ListItemWrapper>
+              <Divider />
+            </Grid>
+          ))}
+          <Grid direction="column" style={{ background: "#c3c3c3", padding: 10 }} xs={12}>Total</Grid>
         </Grid>
-      )}
+      </Grid>
+
     </MainCard>
   );
 };
